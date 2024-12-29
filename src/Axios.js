551d2f1e18d6842c818ffe77cb8bf79ec1c5f089
PrajwalNeupaneDgaf <157794663+PrajwalNeupaneDgaf@users.noreply.github.com>
2024-12-30 00:43:33 +0545

    Update Axios.js

--- src/Axios.js
+++ src/Axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // Create an Axios instance with a custom configuration
 const api = axios.create({
   //baseURL: 'http://localhost:8080/api/',
-  baseURL: 'https://chatbot-backend-v6m5.onrender.com//api/',
+  baseURL: 'https://chatbot-backend-v6m5.onrender.com/api/',
   headers: {
     'Content-Type': 'application/json',
   },
